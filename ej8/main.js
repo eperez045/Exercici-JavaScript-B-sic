27f46aaf96fb419b46461143a194c05d45dbc38c
elena <eperez045@gmail.com>
2020-06-19 11:24:54 +0200

    correcion ej8
    Funcion con 3 parametros

--- ej8/main.js
+++ ej8/main.js
@@ -1,20 +1,22 @@
-function calcula(){
-    let valor1 = parseInt(document.getElementById("valor1").value);
-    let valor2 = parseInt(document.getElementById("valor2").value);
-    let operador = document.getElementById("operador").value;
-    if (operador=="+"){
-        let resultado = valor1+valor2
-        document.getElementById("respuesta").innerHTML = resultado;
-    }else if(operador=="-"){
-        let resultado = valor1-valor2
-        document.getElementById("respuesta").innerHTML = resultado;
-    }else if(operador=="*"){
-        let resultado = valor1*valor2
-        document.getElementById("respuesta").innerHTML = resultado;
-    }else if(operador=="/"){
-        let resultado = valor1/valor2
-        document.getElementById("respuesta").innerHTML = resultado;
-    }else{
-        document.getElementById("respuesta").innerHTML = "Introduzca un operador valido(+,-,*,/)"
-    }
-}
+function calcula (valor1,valor2,operador) {
+
+    var valor1 = parseInt(prompt("Introduce el primer valor"));
+    var valor2 = parseInt(prompt("Introduce el segundo valor"));
+    var opcion = prompt("Elije opción de calculadora \n 1. Suma \n 2.Resta \n 3.Multiplicación \n 4.División");
+    var operador = 0;
+      if (opcion == 1) {
+        operador =  valor1 + valor2;
+        alert("El valor de la suma es: "+operador);
+      }else if (opcion == 2){
+        operador =  valor1 - valor2;
+        alert("El valor de la resta es: "+operador);
+      }else if (opcion == 3){
+        operador =  valor1 * valor2;
+        alert("El valor de la multiplicación es: "+operador);
+      }else if (opcion == 4){
+        operador =  valor1 / valor2;
+        alert("El valor de la división es: "+operador);
+      }else { alert("La opción introducida es errónea")
+      }
+  }
+  calcula();
